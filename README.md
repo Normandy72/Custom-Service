01dba453fbd6d46fb41f795967f1999581de9d10
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-01-08 13:53:37 +0100

    feat: add example of using .provider() and .config(), add more info in README

--- README.md
+++ README.md
@@ -98,3 +98,67 @@ Injected factory function refers to whatever is returned in the factory function
 * can be object literal with a property that's a function that creates something
 * can be a function that creates something
 ***
+
+## 3
+### Custom Services with .provider()
+Steps to use `.provider()` function:
+#### Step 1 - Define Provider Function
+```
+function ServiceProvider(){
+    var provider = this;
+    provider.config = {...};
+
+    provider.$get = function(){
+        var service = new Service(provider.config.prop);
+        return service;
+    };
+}
+```
+#### Step 2 - Register Provider Function With Module
+```
+angular.module('app', [])
+.controller('ctrl', Ctrl)
+.provider('Service', ServiceProvider);
+```
+Name of service (`'Sevice'`) as it will be injected into other services, controllers, etc.
+
+Name of this function __does not__ matter at all.
+
+#### Step 3 - Inject It As Usual
+```
+Ctrl.$inject = ['$scope', 'Service'];
+function Ctrl($scope, Service){
+    Service.someMethod();
+};
+```
+#### Step 4a (Optional) - Register Config Function
+```
+angular.module('app', [])
+.controller('ctrl', Ctrl)
+.provider('Service', ServiceProvider)
+.config(Config);
+```
+Config guaranteed to run before any services, factories or controllers are created.
+
+#### Step 4b (Optional) - Inject Provider Into Config
+```
+...
+.provider('Service', ServiceProvider);
+...
+Config.$inject = ['ServiceProvider'];
+function Config(ServiceProvider){
+    ServiceProvider.config.prop = 'value';
+};
+```
+***
+#### _Summary_
+`.provider()` - most verbose, but most flexible
+* configure factory not just at time of use, but at app bootstrapping
+
+`.provider('name', function)`
+* whatever the 'name' is  - that's what gets injected into other components
+
+`.config()` function gets called _before_ any service, factory, or controller is instantiated
+* therefor, we __can't__ inject any regular components into `.config`
+* we __can__ inject the provider of service with nameProvider
+***
--- app.js
+++ app.js
@@ -54,59 +54,135 @@
 
 // ********************************************************************************
 // ---------- CUSTOM SERVICE THROUGH A FACTORY----------
+// (function(){
+//     'use strict';
+
+//     angular.module('ShoppingListApp', [])
+//     .controller('ShoppingListController1', ShoppingListController1)
+//     .controller('ShoppingListController2', ShoppingListController2)
+//     .factory('ShoppingListFactory', ShoppingListFactory);
+
+//     // List #1 - Controller
+//     ShoppingListController1.$inject = ['ShoppingListFactory'];
+//     function ShoppingListController1(ShoppingListFactory){
+//         var list1 = this;
+
+//         // Use factory to create new shopping list service
+//         var shoppingList = ShoppingListFactory();
+
+//         list1.items = shoppingList.getItem();
+
+//         list1.itemName = '';
+//         list1.itemQuantity = '';
+
+//         list1.addItem = function(){
+//             shoppingList.addItem(list1.itemName, list1.itemQuantity);
+//         };
+
+//         list1.removeItem = function(itemIndex){
+//             shoppingList.removeItem(itemIndex);
+//         }
+//     };
+
+//     // List #2 - Controller
+//     ShoppingListController2.$inject = ['ShoppingListFactory'];
+//     function ShoppingListController2(ShoppingListFactory){
+//         var list2 = this;
+
+//         // Use factory to create new shopping list service
+//         var shoppingList = ShoppingListFactory(3);
+
+//         list2.items = shoppingList.getItem();
+
+//         list2.itemName = '';
+//         list2.itemQuantity = '';
+
+//         list2.addItem = function(){
+//             try{
+//                 shoppingList.addItem(list2.itemName, list2.itemQuantity);
+//             }catch(error){
+//                 list2.errorMessage = error.message;
+//             }            
+//         };
+
+//         list2.removeItem = function(itemIndex){
+//             shoppingList.removeItem(itemIndex);
+//         }
+//     };
+
+//     // If not specified, maxItems assumed unlimited
+//     function ShoppingListService(maxItems){
+//         var service = this;
+        
+//         // list of shopping items
+//         var items = [];
+        
+//         service.addItem = function(itemName, itemQuantity){
+//             if((maxItems === undefined) || (maxItems !== undefined) && (items.length < maxItems)){
+//                 var item = {
+//                     name: itemName,
+//                     quantity: itemQuantity
+//                 };
+//                 items.push(item);
+//             }            
+//             else{
+//                 throw new Error('Max items: (' + maxItems + ') reached.');
+//             }
+//         };
+        
+//         service.removeItem = function(itemIndex){
+//             items.splice(itemIndex, 1);
+//         };
+        
+//         service.getItem = function(){
+//             return items;
+//         };
+//     };
+
+//     function ShoppingListFactory(){
+//         var factory = function(maxItems){
+//             return new ShoppingListService(maxItems);
+//         };
+//         return factory;
+//     };
+
+// })();
+
+
+// ********************************************************************************
+// CUSTOM SERVICE THROUGH A PROVIDER
 (function(){
     'use strict';
 
     angular.module('ShoppingListApp', [])
-    .controller('ShoppingListController1', ShoppingListController1)
-    .controller('ShoppingListController2', ShoppingListController2)
-    .factory('ShoppingListFactory', ShoppingListFactory);
+    .controller('ShoppingListController', ShoppingListController)
+    .provider('ShoppingListService', ShoppingListServiceProvider)
+    .config(Config);
 
-    // List #1 - Controller
-    ShoppingListController1.$inject = ['ShoppingListFactory'];
-    function ShoppingListController1(ShoppingListFactory){
-        var list1 = this;
-
-        // Use factory to create new shopping list service
-        var shoppingList = ShoppingListFactory();
-
-        list1.items = shoppingList.getItem();
-
-        list1.itemName = '';
-        list1.itemQuantity = '';
-
-        list1.addItem = function(){
-            shoppingList.addItem(list1.itemName, list1.itemQuantity);
-        };
-
-        list1.removeItem = function(itemIndex){
-            shoppingList.removeItem(itemIndex);
-        }
+    Config.$inject = ['ShoppingListServiceProvider'];
+    function Config(ShoppingListServiceProvider){
+        ShoppingListServiceProvider.defaults.maxItems = 2;
     };
 
-    // List #2 - Controller
-    ShoppingListController2.$inject = ['ShoppingListFactory'];
-    function ShoppingListController2(ShoppingListFactory){
-        var list2 = this;
+    ShoppingListController.$injection = ['ShoppingListService'];
+    function ShoppingListController(ShoppingListService){
+        var list = this;
 
-        // Use factory to create new shopping list service
-        var shoppingList = ShoppingListFactory(3);
+        list.items = ShoppingListService.getItem();
 
-        list2.items = shoppingList.getItem();
+        list.itemName = '';
+        list.itemQuantity = '';
 
-        list2.itemName = '';
-        list2.itemQuantity = '';
-
-        list2.addItem = function(){
+        list.addItem = function(){
             try{
-                shoppingList.addItem(list2.itemName, list2.itemQuantity);
+                ShoppingListService.addItem(list.itemName, list.itemQuantity);
             }catch(error){
-                list2.errorMessage = error.message;
+                list.errorMessage = error.message;
             }            
         };
-
-        list2.removeItem = function(itemIndex){
-            shoppingList.removeItem(itemIndex);
+            
+        list.removeItem = function(itemIndex){
+            ShoppingListService.removeItem(itemIndex);
         }
     };
 
@@ -139,11 +215,16 @@
         };
     };
 
-    function ShoppingListFactory(){
-        var factory = function(maxItems){
-            return new ShoppingListService(maxItems);
-        };
-        return factory;
-    };
+    function ShoppingListServiceProvider(){
+        var provider = this;
 
+        provider.defaults = {
+            maxItems: 10
+        };
+
+        provider.$get = function(){
+            var shoppingList = new ShoppingListService(provider.defaults.maxItems);
+            return shoppingList;
+        };
+    };
 })();
