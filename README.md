3175d85e31ea808fa2a98b6cbb8db8f949f39c4b
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-01-08 12:00:59 +0100

    feat: add example of using .factory()

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Custom Service
+
+## 1
 ### Controllers's responsibilites
 Use controllers to
 * Set up initial state of $scope
@@ -35,4 +37,64 @@ Custom services instantiated with `.service` method
 * Lazily instantiated (only created if something depends on them)
 
 `.service('name', function)`, treats function as a function constructor
+***
+## 2
+### Factory Design Pattern
+Central place that produces new objects or functions
+* Can produce any type of object, not just a singleton
+* Can be used to produce dynamically customizable services
+
+### Factory vs. Service Confusion
+* `.factory()` is __not__ just another way of creating the same service you can create with `.service()` but it __can be__.
+* `.service()` is also a factory, but a much more limited one compared to `.factory()`. It's a factory that always produces the same type of service - a singleton, without an easy way to configure its behavior.
+
+### Register Service Factory Function
+```
+angular.module('app', [])
+.controller('ctrl', Ctrl)
+.factory('CustomService', CustomService);
+```
+### Service Factory Function - Return Function
+```
+function CustomService(){
+    var factory = function(){
+        return new SomeService();
+    };
+    return factory;
+}
+```
+### Service Factory Function - Return Object Literal
+```
+function CustomService(){
+    var factory = {
+        getSomeService: function(){
+            return new SomeService
+        }
+    };
+    return factory;
+};
+```
+##### Using Object Function Approach
+```
+...
+var someSrv = CustomService();
+someSrv.method();
+```
+##### Using Object Literal Approach
+```
+...
+var someSrv = CustomService.getSomeService();
+someSrv.method();
+```
+***
+#### _Summary_
+`.factory()` allows us to produce any type of object or function
+* that includes a service (even a singleton), but is __not__ limited to
+* `service()` is just a more limited factory
+
+`.factory('name', FactoryFunction)` - name is what's injected
+
+Injected factory function refers to whatever is returned in the factory function
+* can be object literal with a property that's a function that creates something
+* can be a function that creates something
 ***
--- app.js
+++ app.js
@@ -1,52 +1,149 @@
 // ---------- CUSTOM SERVICE ----------
+// (function(){
+//     'use strict';
+
+//     angular.module('ShoppingListApp', [])
+//     .controller('ShoppingListAddController', ShoppingListAddController)
+//     .controller('ShoppingListShowController', ShoppingListShowController)
+//     .service('ShoppingListService', ShoppingListService);
+
+//     ShoppingListAddController.$inject = ['ShoppingListService'];
+//     function ShoppingListAddController(ShoppingListService){
+//         var itemAdder = this;
+//         itemAdder.itemName = '';
+//         itemAdder.itemQuantity = '';
+
+//         itemAdder.addItem = function(){
+//             ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+//         };
+//     };
+
+//     ShoppingListShowController.$inject = ['ShoppingListService'];
+//     function ShoppingListShowController(ShoppingListService){
+//         var showList = this;
+//         showList.items = ShoppingListService.getItem();
+//         showList.removeItem = function(itemIndex){
+//             ShoppingListService.removeItem(itemIndex);
+//         };
+//     };
+
+//     function ShoppingListService(){
+//         var service = this;
+
+//         // list of shopping items
+//         var items = [];
+
+//         service.addItem = function(itemName, itemQuantity){
+//             var item = {
+//                 name: itemName,
+//                 quantity: itemQuantity
+//             };
+//             items.push(item);
+//         };
+
+//         service.removeItem = function(itemIndex){
+//             items.splice(itemIndex, 1);
+//         };
+
+//         service.getItem = function(){
+//             return items;
+//         };
+//     };
+// })();
+
+
+// ********************************************************************************
+// ---------- CUSTOM SERVICE THROUGH A FACTORY----------
 (function(){
     'use strict';
 
     angular.module('ShoppingListApp', [])
-    .controller('ShoppingListAddController', ShoppingListAddController)
-    .controller('ShoppingListShowController', ShoppingListShowController)
-    .service('ShoppingListService', ShoppingListService);
+    .controller('ShoppingListController1', ShoppingListController1)
+    .controller('ShoppingListController2', ShoppingListController2)
+    .factory('ShoppingListFactory', ShoppingListFactory);
 
-    ShoppingListAddController.$inject = ['ShoppingListService'];
-    function ShoppingListAddController(ShoppingListService){
-        var itemAdder = this;
-        itemAdder.itemName = '';
-        itemAdder.itemQuantity = '';
+    // List #1 - Controller
+    ShoppingListController1.$inject = ['ShoppingListFactory'];
+    function ShoppingListController1(ShoppingListFactory){
+        var list1 = this;
 
-        itemAdder.addItem = function(){
-            ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+        // Use factory to create new shopping list service
+        var shoppingList = ShoppingListFactory();
+
+        list1.items = shoppingList.getItem();
+
+        list1.itemName = '';
+        list1.itemQuantity = '';
+
+        list1.addItem = function(){
+            shoppingList.addItem(list1.itemName, list1.itemQuantity);
         };
+
+        list1.removeItem = function(itemIndex){
+            shoppingList.removeItem(itemIndex);
+        }
     };
 
-    ShoppingListShowController.$inject = ['ShoppingListService'];
-    function ShoppingListShowController(ShoppingListService){
-        var showList = this;
-        showList.items = ShoppingListService.getItem();
-        showList.removeItem = function(itemIndex){
-            ShoppingListService.removeItem(itemIndex);
+    // List #2 - Controller
+    ShoppingListController2.$inject = ['ShoppingListFactory'];
+    function ShoppingListController2(ShoppingListFactory){
+        var list2 = this;
+
+        // Use factory to create new shopping list service
+        var shoppingList = ShoppingListFactory(3);
+
+        list2.items = shoppingList.getItem();
+
+        list2.itemName = '';
+        list2.itemQuantity = '';
+
+        list2.addItem = function(){
+            try{
+                shoppingList.addItem(list2.itemName, list2.itemQuantity);
+            }catch(error){
+                list2.errorMessage = error.message;
+            }            
         };
+
+        list2.removeItem = function(itemIndex){
+            shoppingList.removeItem(itemIndex);
+        }
     };
 
-    function ShoppingListService(){
+    // If not specified, maxItems assumed unlimited
+    function ShoppingListService(maxItems){
         var service = this;
-
+        
         // list of shopping items
         var items = [];
-
+        
         service.addItem = function(itemName, itemQuantity){
-            var item = {
-                name: itemName,
-                quantity: itemQuantity
-            };
-            items.push(item);
+            if((maxItems === undefined) || (maxItems !== undefined) && (items.length < maxItems)){
+                var item = {
+                    name: itemName,
+                    quantity: itemQuantity
+                };
+                items.push(item);
+            }            
+            else{
+                throw new Error('Max items: (' + maxItems + ') reached.');
+            }
         };
-
+        
         service.removeItem = function(itemIndex){
             items.splice(itemIndex, 1);
         };
-
+        
         service.getItem = function(){
             return items;
         };
     };
+
+    function ShoppingListFactory(){
+        var factory = function(maxItems){
+            return new ShoppingListService(maxItems);
+        };
+        return factory;
+    };
+
 })();
